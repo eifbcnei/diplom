5167f8844393ed3894e7ded5ed65cc5449debfe6
MaksimY <maxim99nn@gmail.com>
2022-11-01 21:14:06 +0300

    complete task1, subtask2

--- doa_task1.m
+++ doa_task1.m
@@ -1,19 +1,34 @@
-n = 4;
-f = 77e9;
-dlambda = 3e8 / f;
+n = 10;
+dlambda = 0.5;
 num_tgt = 2;
-theta1 = -10;
-theta2 = 10;
+theta1 = deg2rad(-10);
+theta2 = deg2rad(15);
 v = 10;
+v1=v;v2=v;
 sigma = 1;
-l = 1;
-rng('default')
-a = 1 / 2^0.5 * (normrnd(0,sigma, [num_tgt, 1])+normrnd(0,sigma, [num_tgt, 1])*1j);
-s1 = exp(1j*2*3.14*dlambda *(1:n)'* sin(theta1));
-s2 = exp(1j*2*3.14*dlambda *(1:n)'* sin(theta2));
-z = 1 / 2^0.5 * normrnd(0, 1, [n, 1]);
-x = a(1) * s1 + a(2) * s2 + z;
-m = sigma^2 * (eye(n) + v * s1 * s1.' + v * s2 * s2.');
-e = eig(m);
-malt = (1/l) * (x * x.');
+l = 5000;
+rng default;
+malt = zeros(n);
+
+for i = 1:l
+    a1 = (v1 / 2)^0.5 * (normrnd(0,sigma)+normrnd(0,sigma)*1j);
+    a2 = (v2 / 2)^0.5 * (normrnd(0,sigma)+normrnd(0,sigma)*1j);
+    s1 = exp(1j*2*3.14*dlambda *((1:n)-1)' * sin(theta1));
+    s2 = exp(1j*2*3.14*dlambda *((1:n)-1)' * sin(theta2));
+    z = (sigma / 2)^0.5 * (normrnd(0, 1, [n, 1]) + normrnd(0, 1, [n, 1])*1j);
+    x = a1 * s1 + a2 * s2 + z;
+    m = sigma^2 * (eye(n) + v1 * (s1 * s1') + v2 * (s2 * s2'));
+    e = eig(m);
+    malt = malt + x * x';
+    if (i == 1)
+       figure
+       scatter(e, zeros(size(e)), 'ro')
+       hold on;
+       grid on
+    end
+end
+malt = malt / l;
 ealt = eig(malt);
+
+scatter((ealt), zeros(size(ealt)), 'bx')
+hold off
